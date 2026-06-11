2270e708bdf24d4ac41d0b1fddd4664420af5540
zhuobinggang <zhuobinggang@gmail.com>
2026-06-11 16:10:07 +0900

    cool

--- multi_step_decode.py
+++ multi_step_decode.py
@@ -54,7 +54,12 @@ def multi_step_decode(input_ids, attention_mask, bert=None):
         predicted_token_id = logits[0, best_position].argmax(dim=-1).item()
         
         # Convert token id to label (1-5)
-        predicted_label = reversed_dict.get(predicted_token_id, 5)
+        if predicted_token_id in reversed_dict:
+            predicted_label = reversed_dict[predicted_token_id]
+        else:
+            decoded_token = toker.decode(predicted_token_id)
+            print(f"警告: {predicted_token_id} = {decoded_token}, 不在编码字典中, 分配标签 5")
+            predicted_label = 5
         predicted_labels.append(predicted_label)
         
         # Fill the predicted token at this position in the input
@@ -76,8 +81,9 @@ def valid_bert_multi_step(bert=None, split='val'):
     """
     if bert is None:
         bert = default_bert()
+    reversed_dict = reverse_indexs_tokenized()
     
-    paragraphs = sind_only_texts_get_by_split(split)
+    paragraphs = sind_only_texts_get_by_split(split)[:10]
     bert_inputs = sind_data_prepare(paragraphs)
     
     all_predicted_labels = []
@@ -86,6 +92,7 @@ def valid_bert_multi_step(bert=None, split='val'):
     for bert_input in tqdm(bert_inputs):
         predicted_labels = multi_step_decode(bert_input.input_ids, bert_input.attention_mask, bert)
         true_labels = [label for label in bert_input.labels if label != -100]
+        true_labels = [reversed_dict[b] for b in true_labels]
         
         assert len(predicted_labels) == len(true_labels) == 5, "There should be exactly 5 predicted and true labels"
         
@@ -93,11 +100,13 @@ def valid_bert_multi_step(bert=None, split='val'):
         all_true_labels.append(true_labels)
     
     test_result = cal_tau_acc_pmr(all_predicted_labels, all_true_labels, need_fix=False)
+    # print('all predicted labels:\n', all_predicted_labels)
+    # print('all true labels:\n', all_true_labels)
     return test_result
 
 
 # Example usage
 if __name__ == '__main__':
     bert = default_bert()
-    load_checkpoint(bert, './checkpoints/SIND_best_e0.pth')
-    result = valid_bert_multi_step(bert, 'test')
+    load_checkpoint(bert, './checkpoints/SIND_best_e1.pth')
+    result = valid_bert_multi_step(bert, 'val')
