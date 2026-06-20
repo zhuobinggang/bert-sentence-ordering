cff81c96f377571073fda8e4992c76adc1cb3cf8
zhuobinggang <zhuobinggang@gmail.com>
2026-06-20 13:21:54 +0900

    实验rocs random 3 pass

--- three_pass_with_critic.py
+++ three_pass_with_critic.py
@@ -2,8 +2,9 @@ from two_pass_plus import *
 from critic_bert_simple import *
 from critic_bert import resort_paragraph, recover_unsorted_paragraph
 
-def valid_bert_n_pass_random_with_critic(bert, critic, split = 'val', npass = 3):
-    paragraphs = sind_paragraphs(split)
+def valid_bert_n_pass_random_with_critic(bert, critic, split = 'val', npass = 3, paragraphs = None):
+    if paragraphs is None:
+        paragraphs = sind_paragraphs(split)
     all_predicted_labels = []
     all_true_labels = []
     printed = False
@@ -50,8 +51,13 @@ def valid_trained():
     bert = default_trained_bert()
     valid_bert_n_pass_random_with_critic(bert, critic, 'test', npass=3)
     
-def valid_trained_in_folder(search_string = '_vanilla_sind_'):
-    critic = default_critic_model_sind()
+def valid_trained_in_folder(sind = True):
+    search_string = '_vanilla_sind_' if sind else '_vanilla_rocs_'
+    critic = default_critic_model_sind() if sind else default_critic_model_rocs()
+    paragraphs = sind_paragraphs('test') if sind else rocs.dataset_get()['test']
+    taus = []
+    accs = []
+    pmrs = []
     from pathlib import Path
     directory_path = Path("./checkpoints")
     matching_files = [file for file in directory_path.glob(f"*{search_string}*") if file.is_file()]
@@ -60,9 +66,16 @@ def valid_trained_in_folder(search_string = '_vanilla_sind_'):
         load_checkpoint(bert, str(file))
         bert.to(DEVICE)
         bert.eval()
-        result = valid_bert_n_pass_random_with_critic(bert, critic, 'test', npass=3)
+        result = valid_bert_n_pass_random_with_critic(bert, critic, split='do_not_use', npass=3, paragraphs=paragraphs)
+        taus.append(result.tau)
+        accs.append(result.acc)
+        pmrs.append(result.pmr)
         print(f'Model {file}, Test Result: {result}')
         common.logging.warning(f'Model {file}, Test Result: {result}')
+    print('Taus:')
+    common.cal_mean_std(taus)
+    print('Accs:')
+    common.cal_mean_std(accs)
+    print('PMRs:')
+    common.cal_mean_std(pmrs)
 
-def valid_trained_aux_loss_model_in_folder():
-    valid_trained_in_folder(search_string  = '_aux_loss_repeat_')
