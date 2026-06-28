13e695f57e4fba6a6ecb8ef309c02ee6a4fd5829
zhuobinggang <zhuobinggang@gmail.com>
2026-06-28 13:06:07 +0900

    准备计算direct mlm的不一致率

--- critic_randomk.py
+++ critic_randomk.py
@@ -3,55 +3,6 @@ from critic_bert_simple import *
 from critic_bert import resort_paragraph, recover_unsorted_paragraph
 from common import list_equal, list_in
 
-def valid_bert_n_pass_random_with_critic(bert, critic, split = 'val', npass = 3, paragraphs = None, output_details = False):
-    if paragraphs is None:
-        paragraphs = sind_paragraphs(split)
-    all_predicted_labels = []
-    all_true_labels = []
-    printed = False
-    for paragraph in tqdm(paragraphs):
-        best_critic_score = float('-inf')
-        best_predicted_labels = None
-        labels = None
-        predicted_labels = [] # 存储每次解码的 predicted_labels，如果已经有了，就不再评分了
-        # npass次解码
-        for _ in range(npass):
-            random_labels  = add_one(random.sample(range(5), 5))
-            random_paragraph = recover_unsorted_paragraph(paragraph, random_labels)
-            bert_input = create_bert_input_for_shuffled_paragraph(random_paragraph, random_labels)
-            temp_mask_token_5index_logits = get_mask_token_5index_logits(bert_input.input_ids, bert_input.attention_mask, bert)
-            temp_predicted_labels = hungarian_algorithm_best_order(temp_mask_token_5index_logits.cpu().numpy())
-            if not list_in(temp_predicted_labels, predicted_labels): # 性能优化：如果已经有了，就不再评分了
-                predicted_labels.append(temp_predicted_labels)
-                temp_resorted_paragraph = resort_paragraph(random_paragraph, temp_predicted_labels)
-                critic_score = get_critic_score(critic, temp_resorted_paragraph)
-                if critic_score > best_critic_score:
-                    best_critic_score = critic_score
-                    best_predicted_labels = temp_predicted_labels
-                    labels = random_labels
-            if not printed:
-                print(f'Original paragraph: {paragraph}')
-                print(f"Random shuffled paragraph: {random_paragraph}")
-                print(f"True label: {random_labels}")
-                print(f"Predicted label: {temp_predicted_labels}")
-                print(f"Resorted paragraph: {temp_resorted_paragraph}")
-                print(f"Critic score: {critic_score}")
-                printed = True
-        all_predicted_labels.append(best_predicted_labels)
-        all_true_labels.append(labels)
-    # 在修正标签之前计算一次
-    test_result = cal_tau_acc_pmr(all_predicted_labels, all_true_labels, need_fix = False)
-    if not output_details:
-        return test_result
-    else:
-        return {
-            'tau': test_result.tau,
-            'acc': test_result.acc,
-            'pmr': test_result.pmr,
-            'all_predicted_labels': all_predicted_labels,
-            'all_true_labels': all_true_labels
-        }
-
 def default_trained_bert(path = './checkpoints/SIND_best_e1.pth'):
     bert = default_bert()
     load_checkpoint(bert, path)
@@ -139,3 +90,37 @@ def valid_bert_n_pass_random_with_critic(bert, critic, split = 'val', npass = 3,
             'all_predicted_labels': all_predicted_labels,
             'all_true_labels': all_true_labels
         }
+    
+
+def valid_bert_n_pass_coherency(sind = True, split = 'val', npass = 2):
+    if sind:
+        paragraphs = sind_paragraphs(split)
+    else:
+        paragraphs = rocs.dataset_get()[split]
+    from pathlib import Path
+    directory_path = Path("./checkpoints")
+    search_string = '_vanilla_sind_' if sind else '_vanilla_rocs_'
+    matching_files = [file for file in directory_path.glob(f"*{search_string}*") if file.is_file()]
+    for file in matching_files:
+        bert = default_bert()
+        load_checkpoint(bert, str(file))
+        bert.to(DEVICE)
+        bert.eval()
+        print(f'Validating model {file} on coherency...')
+        results = []
+        for paragraph in tqdm(paragraphs):
+            predicted_labels = [] # 存储每次解码的 predicted_labels，如果已经有了，就不再评分了
+            # npass次解码
+            consistent = True
+            for _ in range(npass):
+                random_labels  = add_one(random.sample(range(5), 5))
+                random_paragraph = recover_unsorted_paragraph(paragraph, random_labels)
+                bert_input = create_bert_input_for_shuffled_paragraph(random_paragraph, random_labels)
+                temp_mask_token_5index_logits = get_mask_token_5index_logits(bert_input.input_ids, bert_input.attention_mask, bert)
+                temp_predicted_labels = hungarian_algorithm_best_order(temp_mask_token_5index_logits.cpu().numpy())
+                if not list_in(temp_predicted_labels, predicted_labels):
+                    consistent = False
+                    break
+            results.append(1 if consistent else 0)
+        print(f'Coherency: {sum(results)}/{len(results)} = {sum(results)/len(results)}')
+    
